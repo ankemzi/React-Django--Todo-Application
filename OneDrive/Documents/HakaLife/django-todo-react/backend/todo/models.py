77ac16f4b2062a4094775f5c6cb8c276ee882cc1
ankemzi <nkemzia@gmail.com>
2019-04-20 13:29:23 -0400

    Django Models

--- OneDrive/Documents/HakaLife/django-todo-react/backend/todo/models.py
+++ OneDrive/Documents/HakaLife/django-todo-react/backend/todo/models.py
@@ -1,3 +1,11 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Todo(models.Model):
+    title = models.Charfield(max_length=120)
+    description =  models.TextField()
+    complete = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
